4ad477fa6e80f09e64cbfaf6e21d774233e99be6
ligadobra <russiancmo@gmail.com>
2022-11-29 18:22:42 +0300

    add images loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,13 @@ module.exports = {
           "css-modules-typescript-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
     ],
   },
   resolve: { extensions: ["*", ".js", ".jsx", ".tsx", ".ts"] },
