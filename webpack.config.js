8a29879bb1f937d71697e12624bfa1dc6016ac6c
ligadobra <russiancmo@gmail.com>
2022-12-01 10:19:53 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
