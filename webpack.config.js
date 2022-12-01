55107045b32dac044ba459e0fea175e118260cfc
ligadobra <russiancmo@gmail.com>
2022-12-01 09:56:07 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: 'assets/[hash][ext]',
+    assetModuleFilename: 'static/[hash][ext]',
     clean: true,
   },
   module: {
